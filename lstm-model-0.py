4392843b93a3c4e1b34e59654c3b7deeec89d78c
Ahaan Shah <1ahaanshah@gmail.com>
2024-10-12 17:39:23 -0400

    Added layer plan for the model

---
+++ lstm-model-0.py
@@ -0,0 +1,4 @@
+# First, we need to import the necessary libraries and dataframes.
+# Then, we split data into training and testing and write our function for training the data
+# STRUCTURE:
+# 1. Input Layer -> LSTM 1 (Large context size) -> Dropout -> LSTM 2 (Small context size) -> Dropout -> Dense -> OUT
