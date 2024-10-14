77ed33a0811956a2d7f69d74e17df01abc8414ee
mathlete12345654 <72876939+mathlete12345654@users.noreply.github.com>
2024-10-13 21:36:14 -0400

    updated yfinance_data.py

--- yfinance_data.py
+++ yfinance_data.py
@@ -2,14 +2,17 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#put the names of each company stock here
-#Also input date range
-stock_names = yf.Tickers('AAPL GOOGL TSLA')
+
+# Below is range of time we want to consider. Change this to whatever.
 start_date = "2023-10-02"
 end_date = "2024-10-02"
+# Insert the three stocks (or more) that we want to analyze below
+stock_data = yf.download("AAPL GOOGL TSLA", start_date, end_date)
+# The "Close" below, focuses on the last price which a stock traded during a day.
+# We can change that to other things  depending on what we want to predict
+prices = stock_data["Close"]
 
-stock_data = yf.download("AAPL GOOGL TSLA", period="1mo")
-stock_data.plot(kind='line')
+prices.plot()
 plt.xlabel = ("time")
-plt.ylabel = ("stock price")
+plt.ylabel = ("stock price/close")
 plt.show()
